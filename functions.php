85351c2094878534201ca0bf6624047a8b55f720
KirillTovpinets <kirill.tovpinets@icloud.com>
2024-06-21 17:21:03 -0500

    update sessings

--- functions.php
+++ functions.php
@@ -13,10 +13,40 @@
     )
   );
 
-  add_settings_field( 'myprefix_setting-id',
-	'This is the setting title',
- 	'myprefix_setting_callback_function',
-	'general',
-	'myprefix_settings-section-name',
-	array( 'label_for' => 'myprefix_setting-id' ) );
+  add_action( 'admin_menu', 'add_option_field_to_general_admin_page' );
+
+  function add_option_field_to_general_admin_page() {
+
+	$option_name = 'my_option';
+
+	// register the option
+	register_setting( 'general', $option_name );
+
+	// add a field
+	add_settings_field(
+		'myprefix_setting-id',
+		'Option Name',
+		'myprefix_setting_callback_function',
+		'general',
+		'default',
+		[
+			'id'          => 'myprefix_setting-id',
+			'option_name' => 'my_option'
+		]
+	);
+}
+
+function myprefix_setting_callback_function( $val ) {
+
+	$id = $val['id'];
+	$option_name = $val['option_name'];
+	?>
+	<input
+		type="text"
+		name="<?= $option_name ?>"
+		id="<?= $id ?>"
+		value="<?= esc_attr( get_option( $option_name ) ) ?>"
+	/>
+	<?php
+}
 ?>
