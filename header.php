2cb79ccb977daa373892484ee33ae9a7cfdf3061
KirillTovpinets <kirill.tovpinets@icloud.com>
2024-07-26 09:50:35 -0500

    add style version

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>?version=1.0" type="text/css" />
 <?php wp_head(); ?>
 </head>
 <body class="bg-black">
